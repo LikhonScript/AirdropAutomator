ea4183f6bc87770f01184cb708b4e064908d2f99
Likhon Sheikh <153798447+LikhonScripts@users.noreply.github.com>
2023-12-30 08:19:46 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+bsc_testnet_url = 'https://data-seed-prebsc-1-s1.binance.org:8545'
+contract_address = '0x359d1cf26294fe1c3273ddf82dc5315ba4181ee1'
+owner_private_key = ''  # Leave empty to prompt user
+contract_abi = '""}]'       # Leave empty to prompt user
